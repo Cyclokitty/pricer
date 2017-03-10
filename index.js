0932b553c82499b252f18a2787a00084a657175f
Laura Vingada <laura@vingada.com>
2017-03-10 17:21:24 -0500

    made function for pricer and it works!

--- index.js
+++ index.js
@@ -4,3 +4,24 @@
 // food extra markup 13%
 // electronics extra 2% markup
 // everything else no other markup
+
+module.exports = {
+  pricer: function(cost, staff, materials) {
+    let flatRate = 0.05;
+    let staffRate = 0.012;
+    let materialRate;
+
+    if (materials === 'pharmaceuticals' || materials === 'drugs') {
+      materialRate = 0.075;
+    } else if (materials === 'food') {
+      materialRate = 0.13;
+    } else if (materials === 'electronics') {
+      materialRate = 0.02;
+    } else {
+      materialRate = 0;
+    }
+
+    flatCost = cost + (cost * flatRate);
+    return 'Output: $' + (flatCost + (flatCost * (staff * staffRate)) + (flatCost * materialRate)).toFixed(2);
+  }
+}
