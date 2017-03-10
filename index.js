bca09192cd10046bb58e4fb4cf8551c5eb670c83
Laura Vingada <laura@vingada.com>
2017-03-10 16:53:50 -0500

    pseudocode for pricing function

---
+++ index.js
@@ -0,0 +1,6 @@
+// flat mark up of 5% on all jobs
+// per person markup of 1.2% per person
+// pharm extra markup 7.5%
+// food extra markup 13%
+// electronics extra 2% markup
+// everything else no other markup
