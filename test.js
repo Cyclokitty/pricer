961e2fe6d3b35ca9c76e594cb33d97aa7f5d4878
Laura Vingada <laura@vingada.com>
2017-03-09 19:29:25 -0500

    added mocha and chai testing

---
+++ test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const expect = require('chai').expect;
+const index = require('./index.js');
+
+describe('Does my index.js file exist', () => {
+  it('should exist', () => {
+    expect(fs.readFileSync('./index.js')).to.exist;
+  });
+});
