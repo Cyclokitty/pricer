c1d66b0f96e6d938e021e90aedc9a76a9eec9130
Laura Vingada <laura@vingada.com>
2017-03-11 18:06:26 -0500

    added two more expect statements in my test to cover all 3 examples

--- test.js
+++ test.js
@@ -11,6 +11,12 @@ describe('Does my index.js file exist', () => {
 // test module.exports function
 describe('Correct total made', () => {
   it('should equal $1591.58', () => {
-      expect(price.pricer(1299.99, 3, 'food')).to.equal('Output: $1591.58');
+    expect(price.pricer(1299.99, 3, 'food')).to.equal('Output: $1591.58');
+  });
+  it('should equal $6199.81', () => {
+    expect(price.pricer(5432.00, 1, 'drugs')).to.equal('Output: $6199.81');
+  });
+  it('should equal $6199.88', () => {
+    expect(price.pricer(12456.95, 4, 'books')).to.equal('Output: $13707.63');
   });
 });
